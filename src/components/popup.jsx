d9084ed4cb92f85d6577001d8fc09f2c6f271247
therynamo <groetkentheryn@gmail.com>
2015-12-20 15:39:21 -0600

    Create Popup Boilerplate
    - React
    - Webpack
    - Babel
    - SkeletonCSS

---
+++ src/components/popup.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Popup extends React.Component {
+  render () {
+    return (
+      <div id="container">
+          <h1>
+            This is my plugin :)
+          </h1>
+      </div>
+    );
+  };
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Popup from './components/popup.jsx'
+
+ReactDOM.render(<Popup />, document.getElementById('ReactEntry'));

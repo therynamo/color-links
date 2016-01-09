2723b0fd1d46372a705db9d75252a80aab5d0c05
therynamo <groetkentheryn@gmail.com>
2016-01-09 12:46:52 -0600

    Make Color Buttons For Popup

--- src/components/colorButton.jsx
+++ src/components/colorButton.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 export default class ColorButton extends React.Component {
   render() {
+    const buttonStyle = {
+      color: 'white',
+      background: this.props.color
+    };
+
     return (
       <div>
-        <button onClick={this.props.clickHandler}>
-          {this.props.color}
+        <button onClick={this.props.clickHandler} style={buttonStyle}>
         </button>
       </div>
     );

b5d2749490d7bac1a75cd98d773e2332798db120
therynamo <groetkentheryn@gmail.com>
2016-02-13 22:58:45 +0530

    Finish Build Process
    Dist now populates with a fully functioning extension. When running dev build,
    it will build and populate in public like it has been.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 'use strict';
 const gulp = require('gulp');
-const clean = require('gulp-clean');
+const del = require('del');
+const rename = require('gulp-rename');
 const eslint = require('gulp-eslint');
 const sourcemaps = require('gulp-sourcemaps');
 const concat = require('gulp-concat');
@@ -10,7 +11,12 @@ const gwebpack = require('webpack-stream');
 
 const path = {
   js: ['!node_modules/**', './**/*.js', './**/*.jsx'],
-  contentscripts: ['!node_modules/**', './src/content-scripts/**/*.js']
+  contentscripts: ['!node_modules/**', './src/content-scripts/**/*.js'],
+  images: './public/images/**',
+  styles: './public/styles/**',
+  html: './public/*.html',
+  dist: './dist/',
+  public: './public/'
 };
 
 gulp.task('default', ['lint']);
@@ -23,14 +29,46 @@ gulp.task('lint', () => {
 });
 
 gulp.task('clean', () => {
-  return gulp.src('public/dist', { read: false })
-    .pipe(clean());
+  return del([
+    `${path.dist}**`,
+    `${path.public}dist/**`
+  ]);
+});
+
+gulp.task('copy:manifest', () => {
+  return gulp.src('./prod.manifest.json')
+    .pipe(rename({
+      basename: 'manifest',
+      extname: '.json'
+    }))
+    .pipe(gulp.dest(path.dist));
+});
+
+gulp.task('copy:images', () => {
+  return gulp.src(path.images)
+    .pipe(gulp.dest(path.dist + 'images'));
+});
+
+gulp.task('copy:styles', () => {
+  return gulp.src(path.styles)
+    .pipe(gulp.dest(path.dist + 'styles'));
+});
+
+gulp.task('copy:html', () => {
+  return gulp.src(path.html)
+    .pipe(gulp.dest(path.dist));
 });
 
 gulp.task('webpack:dev', () => {
   return gulp.src('src/index.js')
     .pipe(gwebpack(require('./webpack.config.js', webpack)))
-    .pipe(gulp.dest('./public/dist'));
+    .pipe(gulp.dest(path.public));
+});
+
+gulp.task('webpack:prod', () => {
+  return gulp.src('src/index.js')
+    .pipe(gwebpack(require('./webpack.production.config.js', webpack)))
+    .pipe(gulp.dest(path.dist + 'src'));
 });
 
 gulp.task('build:dev', () => {
@@ -41,17 +79,17 @@ gulp.task('build:dev', () => {
     }))
     .pipe(concat('contentscript.js'))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('public/dist'));
+    .pipe(gulp.dest(path.public + 'dist'));
 });
 
-gulp.task('build:prod', ['clean', 'lint'], () => {
+gulp.task('build:prod', ['lint', 'copy:manifest', 'copy:images', 'copy:styles', 'copy:html'], () => {
   return gulp.src('src/content-scripts/contentscript.js')
     .pipe(babel({
       presets: ['es2015']
     }))
-    .pipe(gulp.dest('public/dist'));
+    .pipe(gulp.dest(path.dist + 'src'));
 });
 
 gulp.task('watch', () => {
-  gulp.watch(path.contentscripts, ['lint', 'build:dev', 'webpack:dev']);
+  gulp.watch(path.js, ['lint', 'build:dev', 'webpack:dev']);
 });

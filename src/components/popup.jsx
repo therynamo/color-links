e623450313e869afc4ca125561d7195b640e593d
therynamo <groetkentheryn@gmail.com>
2016-02-13 13:42:00 +0530

    Add Color Border
    This will add an active color border around the color-picker.
    
    Issues
    ===
    
    Focus is being selected after refresh of popup, however, when you close and re-open
    focus is still brought to first element.
    
    There are two trains of thought here I believe:
    
    1. Accessibility for keyboard users would be correct by placing focus on first element
    1. Design is wrong because focus is so bold, it appears to indicate selected color.
    
    Now since the above improvement is handling active color, the question is if focus
    is indeed to bold, and we need to scale back, or if this implementation is fine.

--- src/components/popup.jsx
+++ src/components/popup.jsx
@@ -1,23 +1,55 @@
 import React from 'react';
+import { findDOMNode } from 'react-dom';
 import ColorButton from './colorButton.jsx';
-
 const colors = ['green', 'blue', 'red', 'orange', 'cyan'];
 
 export default class Popup extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      activeColor: ''
+    };
+  }
+
+  componentDidMount() {
+    this.getActiveColor()
+      .then((activeColor) => {
+        this.setState({ activeColor });
+        findDOMNode(this.refs[activeColor].refs.button).focus();
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   onClickHandler(color) {
     chrome.tabs.query({ active: true, currentWindow: true }, function tabsQuery(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, { color }, function tabsSendMessage() {});
+    });
+    this.setState({ activeColor: color });
+  }
+
+  getActiveColor() {
+    return new Promise((resolve, reject) => {
+      chrome.storage.sync.get('color', (results) => {
+        if (results.color) {
+          resolve(results.color);
+        } else {
+          reject(new Error('Did Not Recieve Color'));
+        }
+      });
     });
   }
 
   render() {
     return (
-      <div className="colorLinks">
+      <div className="colorLinks" style={{ 'border': `solid ${this.state.activeColor} 2px` }}>
         <div className="colorLinks--grid">
           {
             colors.map((color) => {
               return (
                 <ColorButton
+                  ref={color}
                   color={color}
                   clickHandler={this.onClickHandler.bind(this, color)}
                   key={color}

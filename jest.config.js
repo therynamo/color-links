5b2cde4e4f19f477fbe4179ed90978c09da11494
TherynGroetken <theryn.groetken@target.com>
2020-05-12 11:41:54 -0500

    coverage

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   setupFilesAfterEnv: ['./jest/setup.js'],
+  collectCoverage: true,
+  coverageReporters: ['text', 'lcov'],
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/node_modules/**',
+    '!**/jest/**',
+    '!**/dist/**',
+    '!**/content-scripts/**',
+    '!**/static/**',
+  ],
 };

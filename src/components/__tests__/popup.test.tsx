165cbce80f73b82dc5d03b76d7a9ceb6bf2ea4b2
TherynGroetken <theryn.groetken@target.com>
2020-05-15 08:40:11 -0500

    click tests

--- src/components/__tests__/popup.test.tsx
+++ src/components/__tests__/popup.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, RenderResult, waitFor } from '@testing-library/react';
+import { render, RenderResult, waitFor, fireEvent } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 
 import * as chromeHelpers from '../../helpers/chrome';
@@ -53,5 +53,53 @@ describe('Popup', () => {
 
     expect(customColor).toHaveClass('colorLinks--button active');
     await waitFor(() => expect(customForm.length).toEqual(1), { container });
+  });
+
+  it('should show a custom input when the custom color button is clicked', async () => {
+    let utils = {} as RenderResult;
+
+    act(async () => {
+      utils = render(<Popup />);
+    });
+
+    const { queryAllByLabelText, queryByLabelText } = utils;
+    (chromeHelpers.getActiveColor as jest.Mock).mockResolvedValue(colors[0]);
+
+    await waitFor(() => expect(chromeHelpers.getActiveColor).toHaveBeenCalled());
+
+    const colorButtons = queryAllByLabelText('save color');
+    const customColorButton = queryByLabelText('custom color');
+    const customForm = queryAllByLabelText('custom form');
+
+    expect(colorButtons[0]).toHaveClass('colorLinks--button active');
+    expect(customForm.length).toEqual(0);
+
+    fireEvent.click(customColorButton);
+
+    waitFor(() => expect(customForm.length).toEqual(1));
+  });
+
+  it('should save a color when a color button is clicked', async () => {
+    let utils = {} as RenderResult;
+
+    act(async () => {
+      utils = render(<Popup />);
+    });
+
+    const { queryAllByLabelText } = utils;
+    (chromeHelpers.getActiveColor as jest.Mock).mockResolvedValue(colors[0]);
+
+    await waitFor(() => expect(chromeHelpers.getActiveColor).toHaveBeenCalled());
+
+    const colorButtons = queryAllByLabelText('save color');
+    const customForm = queryAllByLabelText('custom form');
+
+    expect(colorButtons[0]).toHaveClass('colorLinks--button active');
+    expect(customForm.length).toEqual(0);
+
+    fireEvent.click(colorButtons[1]);
+
+    expect(colorButtons[0]).toHaveClass('colorLinks--button');
+    expect(colorButtons[1]).toHaveClass('colorLinks--button active');
   });
 });

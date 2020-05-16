5db8cb723103dc60a7d6d40c8bd8a2f8bac986e5
TherynGroetken <theryn.groetken@target.com>
2020-05-16 11:35:25 -0500

    remove index from coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   setupFilesAfterEnv: ['./jest/setup.js'],
   collectCoverage: true,
   coverageReporters: ['text', 'lcov'],
+  coveragePathIgnorePatterns: ['<rootDir>/src/popup.tsx'],
   collectCoverageFrom: [
     '**/*.{ts,tsx}',
     '!**/node_modules/**',

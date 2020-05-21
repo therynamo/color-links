3ac404e699828bf0a97f7bbc2cfe317b8bc75817
Theryn Groetken <therynamo@users.noreply.github.com>
2020-05-21 09:47:55 -0500

    fix: Override Page Styles (#29)
    * fix: Override Page Styles
    
    * add tests for content script
    
    * ignore content script itself

--- src/content-scripts/contentscript.ts
+++ src/content-scripts/contentscript.ts
@@ -1,57 +1,4 @@
-const displayStyles = function displayStyles(location) {
-  return new Promise((resolve) => {
-    chrome.storage.sync.get("whitelist", (result) => {
-      const arr = result.whitelist && result.whitelist.length ? result.whitelist : [];
-      const { origin } = location;
-      const isWhitelisted = arr.filter((url) => url === origin);
-
-      resolve(isWhitelisted.length);
-    });
-  });
-};
-
-const initializeStylesheet = function initializeStylesheet() {
-  const styleElement = document.createElement("style");
-  styleElement.setAttribute("id", "colorLinks");
-
-  document.head.appendChild(styleElement);
-  chrome.storage.sync.get("color", (results) => {
-    if (results.color) {
-      styleElement.sheet.insertRule(
-        `a:visited {color: "${results.color}";}`,
-        0,
-      );
-      return;
-    }
-    styleElement.sheet.insertRule("a:visited {}", 0);
-  });
-};
-
-const getStyleSheet = function getStyleSheet() {
-  const styleElement = document.getElementById("colorLinks");
-  const styleSheet = styleElement ? styleElement.sheet : undefined;
-
-  return styleSheet;
-};
-
-const setStyleSheet = function setStyleSheet(color) {
-  const styleSheet = getStyleSheet();
-
-  if (styleSheet) {
-    styleSheet.deleteRule(0);
-    styleSheet.insertRule(`a:visited { color:"${color}"}`, 0);
-    chrome.storage.sync.set({ color }, () => {});
-  }
-};
-
-const colorListener = function colorListener(request) {
-  // eslint-disable-line no-unused-vars
-  const { color } = request;
-
-  if (color) {
-    setStyleSheet(color);
-  }
-};
+import { colorListener, displayStyles, initializeStylesheet } from './helpers';
 
 displayStyles(window.location)
   .then((urls) => {
@@ -60,5 +7,4 @@ displayStyles(window.location)
     initializeStylesheet();
     chrome.runtime.onMessage.addListener(colorListener);
   })
-  /* eslint-disable-next-line no-console */
   .catch((err) => console.log(err));

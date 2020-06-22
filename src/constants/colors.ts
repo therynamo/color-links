5bf37fa2a3e5cf4f53c0ea8571726cbcc997211d
Matt Duffin <matthew.duffin@gmail.com>
2020-06-21 22:59:12 -0500

    added enum

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,2 +1,16 @@
 
-export const COLORS: Array<string> = ['#37d67a', '#2ccce4', '#06A77D', '#ff8a65', '#1E91D6'];
+enum COLOR_TYPES {
+  shamrock = '#37d67a',
+  cyan = '#2ccce4',
+  turquoise = '#06A77D',
+  salmon = '#ff8a65',
+  blue = '#1E91D6'
+}
+
+export const COLORS = [
+  COLOR_TYPES.shamrock,
+  COLOR_TYPES.cyan,
+  COLOR_TYPES.turquoise,
+  COLOR_TYPES.salmon,
+  COLOR_TYPES.blue
+];
